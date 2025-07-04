4131fc09810c6ff958a29cc88e1edcf7525edb2e
David Bayode <david4bay@gmail.com>
2025-07-05 00:59:11 +0100

    made header responsive

--- tom_frontend/src/components/Header.tsx
+++ tom_frontend/src/components/Header.tsx
@@ -14,12 +14,24 @@ export default function Header() {
                     <img src="#" alt="header image" className="header__image" />
                     <img src="#" alt="header image" className="header__image" />
                 </div>
-            <h2>
-                Browse. Download. Share your favorite pictures.
-            </h2>
-            <p>
-                The Interactive Gallery collates images from Unsplash API.
-            </p>
+                <div className="header__text">
+                    <h2>
+                        Browse. Download. Share your favorite pictures.
+                    </h2>
+                    <p>
+                        The Interactive Gallery collates images from Unsplash API.
+                    </p>
+                </div>
+                <div>
+                     <ul className="header__links">
+                        <li className="preview button">
+                            Preview
+                        </li>
+                        <li className="auth button">
+                            Signin/Signup
+                        </li>
+                    </ul>
+                </div>
             </div>
         </header>
     )
