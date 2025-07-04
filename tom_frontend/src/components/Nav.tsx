7fc97bac06bbd434ae7c4f0060da7596a3eedfc0
David Bayode <david4bay@gmail.com>
2025-07-04 23:21:41 +0100

    updated nav design

--- tom_frontend/src/components/Nav.tsx
+++ tom_frontend/src/components/Nav.tsx
@@ -4,16 +4,16 @@ export default function Nav() {
     return (
     <nav className="nav__menu">
         <ul className="nav__items">
-            <li className="nav__logo">
-                <strong>TIG</strong>
-                <span>- The Interactive Gallery</span>
+            <li className="nav__logo button">
+                <strong className="logo__short">TIG</strong>
+                <span className="logo__long"> - The Interactive Gallery</span>
             </li>
         </ul>
         <ul className="nav__items">
-            <li>
+            <li className="preview button">
                 Preview
             </li>
-            <li>
+            <li className="auth button">
                 Signin/Signup
             </li>
         </ul>
