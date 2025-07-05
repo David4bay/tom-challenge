de159ac413fc2078f3fdf78a28fd40e4ad3a405c
David Bayode <david4bay@gmail.com>
2025-07-05 16:19:18 +0100

    building signuser component

--- tom_frontend/src/components/SignUser.tsx
+++ tom_frontend/src/components/SignUser.tsx
@@ -1,10 +1,44 @@
+import { useEffect, useState } from "react"
 import Nav from "./Nav";
 
-
 export default function SignUser() {
+
+    
+    const [signup, setSignUp] = useState(false)
+    
+    useEffect(() => {
+        if (signup) {
+            document.title = "Sign up"
+        } else {
+            document.title = "Sign in"
+        }
+
+    }, [signup])
+
     return (
         <>
         <Nav />
+        <section className="form__container">
+            {
+                signup ? (
+
+                ) : (
+        <form>
+            <legend>Sign up</legend>
+            <fieldset>
+            <label htmlFor="username">Username</label>
+            <input type="text" />
+            </fieldset>
+            <fieldset>
+            <label htmlFor="password">Password</label>
+            <input type="password" />
+            </fieldset>
+            <input type="button" />
+            <p></p>
+        </form>
+                )
+            }
+        </section>
         </>
     )
 }
