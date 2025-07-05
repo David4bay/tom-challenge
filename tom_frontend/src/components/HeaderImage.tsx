6a7ef7aef4458e1b42634befbcbac75249fd705d
David Bayode <david4bay@gmail.com>
2025-07-05 16:55:20 +0100

    building signin/signup component #2

--- tom_frontend/src/components/HeaderImage.tsx
+++ tom_frontend/src/components/HeaderImage.tsx
@@ -1,3 +1,7 @@
+import { useEffect, useState } from "react"
+import ImageIcon from "./SVG/ImageIcon"
+import ImageIconFocused from "./SVG/ImageIconFocused"
+
 interface HeaderImageProps {
     urls: {
         raw?: string
@@ -10,7 +14,19 @@ interface HeaderImageProps {
 }
 
 export default function HeaderImage({ urls, alt_description, styling }: HeaderImageProps) {
+
+    const [hover, setHover] = useState(false)
+
+    useEffect(() => {
+        document.addEventListener("mouseenter", function(e) {
+            e.preventDefault()
+            setHover(!hover)
+        })
+    })
     return (
+        <div className="headerImage__wrapper">
         <img src={urls.regular} alt={alt_description} className={`header__image ${styling}`} />
+        {hover ? <ImageIcon /> : <ImageIconFocused />}
+        </div>
     )
 }
--- tom_frontend/src/components/SignUser.tsx
+++ tom_frontend/src/components/SignUser.tsx
@@ -5,6 +5,7 @@ export default function SignUser() {
 
     
     const [signup, setSignUp] = useState(false)
+    const [showPassword, setShowPassword] = useState(false)
     
     useEffect(() => {
         if (signup) {
@@ -12,7 +13,6 @@ export default function SignUser() {
         } else {
             document.title = "Sign in"
         }
-
     }, [signup])
 
     return (
@@ -21,21 +21,41 @@ export default function SignUser() {
         <section className="form__container">
             {
                 signup ? (
-
+                     <form>
+                        <legend>Sign up</legend>
+                            <fieldset>
+                            <label htmlFor="username">Username</label>
+                            <input type="text" />
+                            </fieldset>
+                            <fieldset>
+                            <label htmlFor="email">Email</label>
+                            <input type="email" />
+                            </fieldset>
+                            <fieldset>
+                            <label htmlFor="password">Password</label>
+                            <input type={!showPassword ? "password" : "text"} />
+                            <button type="button" onClick={() => setShowPassword(!showPassword)}>i</button>
+                            </fieldset>
+                            <button type="button" onClick={() => setSignUp(!signup)} className="auth__button">Sign up</button>
+                            <div>
+                    <p>Have an account? </p><button> Login </button>
+                            </div>
+                </form>
                 ) : (
-        <form>
-            <legend>Sign up</legend>
-            <fieldset>
-            <label htmlFor="username">Username</label>
-            <input type="text" />
-            </fieldset>
-            <fieldset>
-            <label htmlFor="password">Password</label>
-            <input type="password" />
-            </fieldset>
-            <input type="button" />
-            <p></p>
-        </form>
+                <form>
+                    <legend>Sign in</legend>
+                        <fieldset>
+                            <label htmlFor="username">Username</label>
+                            <input type="text" />
+                            </fieldset>
+                            <fieldset>
+                            <label htmlFor="password">Password</label>
+                            <input type={!showPassword ? "password" : "text"} />
+                            <button type="button" onClick={() => setShowPassword(!showPassword)}>i</button>
+                            </fieldset>
+                            <button type="button" onClick={() => setSignUp(!signup)} className="auth__button">Sign in</button>
+                    <p></p>
+                </form>
                 )
             }
         </section>
